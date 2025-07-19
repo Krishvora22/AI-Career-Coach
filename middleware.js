6f2f862151cc8cd93b4b6aa00416217e181cdfb3
Krish Vora <krishvora22@gmail.com>
2025-07-19 18:00:05 +0530

    Update

--- middleware.js
+++ middleware.js
@@ -1,15 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher([
+const isProtectedRoute = createRouteMatcher(
   "/dashboard(.*)",
   "/resume(.*)",
-  "/interview(.*)",
   "/ai-cover-letter(.*)",
-  "/onboarding(.*)",
-]);
-
-
+  "/interview(.*)",
+  "/onboarding(.*)"
+);
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
@@ -17,17 +15,14 @@ export default clerkMiddleware(async (auth, req) => {
     const { redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
-
   return NextResponse.next();
 });
-
-
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
